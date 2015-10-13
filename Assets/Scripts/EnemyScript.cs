8d9390caa5a64761e03fa79cf044b1ad142e553c
kouroshnaderi <kourosh.naderi@aalto.fi>
2015-10-13 17:20:40 +0300

    Navigation mesh created. bomb effect corrected.

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+    public Vector3 TragetPos;
+    NavMeshAgent m_Agent;
+
+	// Use this for initialization
+	void Start () 
+    {
+        m_Agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        m_Agent.destination = TragetPos;
+	}
+}
