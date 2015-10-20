2cde826e128dd389b5d1859271ea7558594223df
OmarTheBest <omarthebest@me.com>
2015-10-20 19:07:12 +0300

    Push that shit

---
+++ Assets/Scripts/EscapeDoor.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EscapeDoor : MonoBehaviour {
+
+    private LevelManager levelManager;
+    private DestructibleWall[] walls;
+
+    private bool isInit = false;
+
+    public Text winningText;
+
+	// Use this for initialization
+	void Start () {
+        levelManager = FindObjectOfType<LevelManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isInit)
+        {
+            walls = FindObjectsOfType<DestructibleWall>();
+            PlaceTheEscapeRandomly();
+            isInit = true;
+        }
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerController>() && isInit)
+        {
+            Debug.Log("Player enter trigger");
+            winningText.text = "You Win!";
+            Invoke("Progress", 3f);
+        }
+    }
+
+    void PlaceTheEscapeRandomly()
+    {
+        int random = Random.Range(0, walls.Length);
+        transform.position = walls[random].transform.position;
+    }
+
+    void Progress()
+    {
+        levelManager.LoadNextLevel();
+    }
+}
---
+++ Assets/Scripts/UIBombCount.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIBombCount : MonoBehaviour
+{
+
+    public Text text;
+    private int numberOfBombs;
+
+    // Use this for initialization
+    void Start()
+    {
+        numberOfBombs = FindObjectOfType<PlayerController>().maxNumberOfBombs;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateText();
+    }
+
+    void UpdateText()
+    {
+        numberOfBombs = FindObjectOfType<PlayerController>().maxNumberOfBombs;
+        text.text = "Bombs: " + numberOfBombs;
+
+    }
+}
